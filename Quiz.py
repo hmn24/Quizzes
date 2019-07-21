f1b9e2b70072a4e9f6be9c11b8660188ff7cd191
Hai Ming <34784358+hmn24@users.noreply.github.com>
2019-07-21 19:02:48 +0800

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -7,15 +7,16 @@ val = {}
 # Function to define recursion which utilises memoisation
 def getUmbrellas(n, p):
     if n == 0: return 0
-    if n in val.keys(): return val[n]
+    hashkey = (n, hash(tuple(p)))
+    if hashkey in val.keys(): return val[hashkey]
     res = sys.maxsize
     for j in p:
         if j <= n:
             op = getUmbrellas(n - j, p) 
             if (op != sys.maxsize) and (op + 1) < res:
                 res = op + 1            
-    val[n] = res
-    return res
+                val[hashkey] = res
+    return res 
 
 # Question to quiz
 # getUmbrellas(8, [3,5])
