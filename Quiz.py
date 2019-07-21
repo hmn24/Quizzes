c57d592d9542631affaf638a5e2d335175ecf32c
Hai Ming <34784358+hmn24@users.noreply.github.com>
2019-07-21 18:42:55 +0800

    Add files via upload

--- Quiz.py
+++ Quiz.py
@@ -1,3 +1,4 @@
+### Quiz 1
 import sys
 
 # For memoisation purposes
@@ -18,3 +19,33 @@ def getUmbrellas(n, p):
 
 # Question to quiz
 # getUmbrellas(8, [3,5])
+
+
+### Quiz 2
+n = 6
+r = 3
+ar = [1, 3, 9, 9, 27, 81]
+
+from collections import defaultdict
+
+def count_quadlets(ar,r):
+    v2 = defaultdict(int)
+    v3 = defaultdict(int)
+    v4 = defaultdict(int)
+    count = 0
+    for k in ar:
+        count += v4[k]
+        v4[k*r] += v3[k]
+        v3[k*r] += v2[k]
+        v2[k*r] += 1
+    return count
+
+def count_triplets(ar,r):
+    v2 = defaultdict(int)
+    v3 = defaultdict(int)
+    count = 0
+    for k in ar:
+        count += v3[k]
+        v3[k*r] += v2[k]
+        v2[k*r] += 1
+    return count
